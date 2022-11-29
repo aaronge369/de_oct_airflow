2e6b67dd3d654e263bd49feb2e37a3b13a327414
aaronge <118867156+aaronge369@users.noreply.github.com>
2022-11-29 16:22:12 -0500

    Update user_query_rt_rpt.py
    Update database

--- dags/user_query_rt_rpt.py
+++ dags/user_query_rt_rpt.py
@@ -10,7 +10,7 @@ from airflow.providers.snowflake.operators.snowflake import SnowflakeCheckOperat
 
 
 SNOWFLAKE_CONN_ID = 'snowflake_conn'
-SNOWFLAKE_DATABASE = 'beaconfire'
+SNOWFLAKE_DATABASE = 'etl_af'
 SNOWFLAKE_SCHEMA = 'prod_db'
 
 SNOWFLAKE_ROLE = 'AW_developer'
